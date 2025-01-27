76ff99fd7ab969ab237eaae80f87cf0574aa65aa
Julian Barragan Verano <julbarg@gmail.com>
2025-01-27 09:28:45 -0500

    Performance considetations

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,8 @@
 import React, { useRef, useEffect } from 'react';
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import { useHistory } from 'react-router-dom';
 
-const MarketingApp = () => {
+const AuthApp = () => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -24,5 +24,5 @@ const MarketingApp = () => {
   return <div ref={ref} />;
 }
 
-export default MarketingApp;
+export default AuthApp;
 
